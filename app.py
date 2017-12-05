b55c01f1c9396f3eff164df6a61eaa196872e79b
Aryan Raj <aryan5416@gmail.com>
2017-12-05 22:07:29 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-	return 'Yo, its working!'
+    return 'Yo, its working!'
+
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
