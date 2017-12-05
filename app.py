ed37cbd3ec71b99efeca67b6ee5bac8513b0603e
Aryan Raj <aryan5416@gmail.com>
2017-12-05 23:27:52 +0530

    auth added

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
-@app.route('/')
-def index():
-    return 'Yo, its working!'
+@app.route('/', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@auth.get_password
+def get_password(username):
+    if username == 'aryan':
+        return '3003'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 if __name__ == "__main__":
